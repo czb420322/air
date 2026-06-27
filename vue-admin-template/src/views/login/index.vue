faea9bc2d0c9c69b88fcb00ad9d75efb0dacd166
18120574956 <2751470984@qq.com>
2026-06-27 15:44:20 +0800

    ``` feat(login): 更新登录页面UI设计并国际化错误提示信息
    - 替换登录标题为美的集团logo
    - 将登录按钮文本从"Login"改为"登录"
    - 错误验证消息翻译为中文：用户名验证错误和密码长度验证错误
    - 调整表单样式：更改背景色、边框样式和布局结构
    - 添加新的登录背景图片和调整表单位置
    - 修改输入框颜色和样式以提升用户体验
    ```

--- vue-admin-template/src/views/login/index.vue
+++ vue-admin-template/src/views/login/index.vue
@@ -1,52 +1,35 @@
 <template>
   <div class="login-container">
-    <el-form ref="loginForm" :model="loginForm" :rules="loginRules" class="login-form" auto-complete="on" label-position="left">
+    <el-form ref="loginForm" :model="loginForm" :rules="loginRules" class="login-form" auto-complete="on"
+      label-position="left">
 
       <div class="title-container">
-        <h3 class="title">Login Form</h3>
+        <h3 class="title">
+          <img src="https://static-btri.midea.com/mfs/1023/1690440473891.svg" />
+        </h3>
       </div>
 
       <el-form-item prop="username">
         <span class="svg-container">
           <svg-icon icon-class="user" />
         </span>
-        <el-input
-          ref="username"
-          v-model="loginForm.username"
-          placeholder="Username"
-          name="username"
-          type="text"
-          tabindex="1"
-          auto-complete="on"
-        />
+        <el-input ref="username" v-model="loginForm.username" placeholder="Username" name="username" type="text"
+          tabindex="1" auto-complete="on" />
       </el-form-item>
 
       <el-form-item prop="password">
         <span class="svg-container">
           <svg-icon icon-class="password" />
         </span>
-        <el-input
-          :key="passwordType"
-          ref="password"
-          v-model="loginForm.password"
-          :type="passwordType"
-          placeholder="Password"
-          name="password"
-          tabindex="2"
-          auto-complete="on"
-          @keyup.enter.native="handleLogin"
-        />
+        <el-input :key="passwordType" ref="password" v-model="loginForm.password" :type="passwordType"
+          placeholder="Password" name="password" tabindex="2" auto-complete="on" @keyup.enter.native="handleLogin" />
         <span class="show-pwd" @click="showPwd">
           <svg-icon :icon-class="passwordType === 'password' ? 'eye' : 'eye-open'" />
         </span>
       </el-form-item>
 
-      <el-button :loading="loading" type="primary" style="width:100%;margin-bottom:30px;" @click.native.prevent="handleLogin">Login</el-button>
-
-      <div class="tips">
-        <span style="margin-right:20px;">username: admin</span>
-        <span> password: any</span>
-      </div>
+      <el-button :loading="loading" type="primary" style="width:100%;margin-bottom:30px;"
+        @click.native.prevent="handleLogin">登录</el-button>
 
     </el-form>
   </div>
@@ -60,14 +43,14 @@ export default {
   data() {
     const validateUsername = (rule, value, callback) => {
       if (!validUsername(value)) {
-        callback(new Error('Please enter the correct user name'))
+        callback(new Error('请输入正确的用户名'))
       } else {
         callback()
       }
     }
     const validatePassword = (rule, value, callback) => {
       if (value.length < 6) {
-        callback(new Error('The password can not be less than 6 digits'))
+        callback(new Error('密码不能少于6位'))
       } else {
         callback()
       }
@@ -88,7 +71,7 @@ export default {
   },
   watch: {
     $route: {
-      handler: function(route) {
+      handler: function (route) {
         this.redirect = route.query && route.query.redirect
       },
       immediate: true
@@ -129,8 +112,8 @@ export default {
 /* 修复input 背景不协调 和光标变色 */
 /* Detail see https://github.com/PanJiaChen/vue-element-admin/pull/927 */
 
-$bg:#283443;
-$light_gray:#fff;
+$bg: #283443;
+$light_gray: #fff;
 $cursor: #fff;
 
 @supports (-webkit-mask: none) and (not (cater-color: $cursor)) {
@@ -152,7 +135,7 @@ $cursor: #fff;
       -webkit-appearance: none;
       border-radius: 0px;
       padding: 12px 5px 12px 15px;
-      color: $light_gray;
+      color: #000;
       height: 47px;
       caret-color: $cursor;
 
@@ -168,28 +151,39 @@ $cursor: #fff;
     background: rgba(0, 0, 0, 0.1);
     border-radius: 5px;
     color: #454545;
+    background-color: #fff;
+    border: 1px solid #8e9ab4;
+    border-radius: 4px;
+    transition: all .3s;
   }
 }
 </style>
 
 <style lang="scss" scoped>
-$bg:#2d3a4b;
-$dark_gray:#889aa4;
-$light_gray:#eee;
+$bg: #2d3a4b;
+$dark_gray: #889aa4;
+$light_gray: #eee;
 
 .login-container {
   min-height: 100%;
   width: 100%;
-  background-color: $bg;
+  background: url(https://static-btri.midea.com/images/apps/sso/login-back.jpg) no-repeat;
+  background-size: 100% 100%;
   overflow: hidden;
+  display: flex;
+  justify-content: flex-end;
 
   .login-form {
     position: relative;
-    width: 520px;
-    max-width: 100%;
-    padding: 160px 35px 0;
-    margin: 0 auto;
+    width: 380px;
+    margin: auto 0;
+    margin-right: 110px;
+    padding: 30px 40px 48px;
     overflow: hidden;
+    background-color: #fff;
+    border-radius: 8px;
+    box-shadow: 0 6px 12px hsla(0, 0%, 84.7%, .12);
+
   }
 
   .tips {
@@ -214,13 +208,22 @@ $light_gray:#eee;
 
   .title-container {
     position: relative;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
 
     .title {
+      width: 198px;
+      height: 80px;
+      padding-top: 10px;
+      padding-bottom: 20px;
       font-size: 26px;
-      color: $light_gray;
-      margin: 0px auto 40px auto;
-      text-align: center;
-      font-weight: bold;
+
+      // color: $light_gray;
+      // margin: 0px auto 40px auto;
+      // text-align: center;
+      // font-weight: bold;
     }
   }
 
